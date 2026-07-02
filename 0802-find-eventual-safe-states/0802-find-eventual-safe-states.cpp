59a22ccc11234beb1e3dcfe4c698fd5de1976977
piratehunter-zoro1999 <kushpindersingh15@gmail.com>
2026-07-03 00:31:04 +0530

    Time: 0 ms (100%), Space: 51.9 MB (84.06%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     
-     bool isCycle(int src,vector<vector<int>> & graph,vector<bool>& vis, vector<bool> & rec){
+     bool isCycle(int src,vector<vector<int>> & graph,vector<bool>& vis, vector<bool> & rec,vector<int> &code){
+          
+
+          if(code[src]==1) return false;
+          if(code[src]==2) return true;
+          
           vis[src]= true;
           rec[src]= true;
-
+        
           for(int v : graph[src]){
             if(!vis[v]){
-                if(isCycle(v,graph,vis,rec)){
+                if(isCycle(v,graph,vis,rec,code)){
+                    code[v]=2;
                     return true;
                 }
             }else if (rec[v]) {
+                code[v]=2;
                 return true;
             }
           }
+          code[src]=1;
           rec[src]= false;
 
           return false;
@@ -22,11 +30,12 @@ public:
         
         int n = graph.size();
         vector<int> safeNodes;
-        
+        vector<bool> vis(n,false);
+        vector<bool> rec(n,false);
+        vector<int> code(n,0);
         for(int i =0 ;i<n;i++){
-            vector<bool> vis(n,false);
-            vector<bool> rec(n,false);
-            if(!isCycle(i,graph,vis,rec)){
+            
+            if(!isCycle(i,graph,vis,rec,code)){
                   safeNodes.push_back(i);
             }
         }
