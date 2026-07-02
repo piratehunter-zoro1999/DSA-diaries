107aa1b53d3b4d92b4b252c61c57b48ac30a85d7
piratehunter-zoro1999 <kushpindersingh15@gmail.com>
2026-07-03 01:09:52 +0530

    Time: 8 ms (75.43%), Space: 51.8 MB (91.56%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     
-     bool isCycle(int src,vector<vector<int>> & graph,vector<bool>& vis, vector<bool> & rec,vector<int> &code){
+     bool isCycle(int src,vector<vector<int>> & graph, vector<bool> & rec,vector<int> &code){
           
+    // code 0-unkown
+    //      1-safe
+    //      2-unsafe
 
           if(code[src]==1) return false;
           if(code[src]==2) return true;
-          
-          vis[src]= true;
+
+         
           rec[src]= true;
         
           for(int v : graph[src]){
-            if(!vis[v]){
-                if(isCycle(v,graph,vis,rec,code)){
-                    code[v]=2;
+             if (rec[v]) { // cycle
+                code[src]=2;
+                rec[src]= false;
+                return true;
+            }else if(code[v]==0){ // recursion
+                if(isCycle(v,graph,rec,code)){
+                    code[src]=2;
+                    rec[src]= false;
                     return true;
                 }
-            }else if (rec[v]) {
-                code[v]=2;
+            }if(code[v]==2){
+                code[src]=2;
                 return true;
+                
             }
+           
           }
           code[src]=1;
           rec[src]= false;
@@ -30,12 +40,11 @@ public:
         
         int n = graph.size();
         vector<int> safeNodes;
-        vector<bool> vis(n,false);
         vector<bool> rec(n,false);
         vector<int> code(n,0);
         for(int i =0 ;i<n;i++){
             
-            if(!isCycle(i,graph,vis,rec,code)){
+            if(!isCycle(i,graph,rec,code)){
                   safeNodes.push_back(i);
             }
         }
