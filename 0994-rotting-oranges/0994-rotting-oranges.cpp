beb35b78bc022f6848c2128961550a94b0673e8e
piratehunter-zoro1999 <kushpindersingh15@gmail.com>
2026-07-14 00:02:26 +0530

    Time: 0 ms (100%), Space: 17.1 MB (40.07%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,52 +1,57 @@
 class Solution {
 public:
-    queue<pair<int,int>> q;
-    void check(vector<vector<int>> & grid,int i,int j,int n, int m){
-
-        vector<pair<int,int>> ways={{i+1,j},{i,j+1},{i-1,j},{i,j-1}};
-
-        for( auto dir : ways){
-            if(dir.first < 0 || dir.second <0 || dir.first >=n || dir.second >=m) continue;
-            if(grid[dir.first][dir.second]==1){
-                q.push(dir);
-                grid[dir.first][dir.second] =2;
-            }
-        }
-    }
     int orangesRotting(vector<vector<int>>& grid) {
-        int n= grid.size();
-        int m = grid[0].size();
-        int count=0;
-        for(int i=0 ;i<n ;i++){
-            for(int j=0;j<m;j++){
+        int m= grid.size();
+        int n = grid[0].size();
+        queue<pair<int,int>> q;
+        
+        bool flag=false;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                   q.push({i,j});
+                    q.push({i,j});
                 }
-                if(grid[i][j]==1) count++;
+                if(grid[i][j]==1) flag=true;
             }
         }
-        if(q.empty() && count == 0) return 0 ;
-        int min=-1;
+
+        if(q.size()==0 && flag ) return -1;
+        if(!flag) return 0;
+
+        int count=-1;
         while(!q.empty()){
             int size=q.size();
-
-            for(int i=0 ; i<size;i++){
-                auto u = q.front();
+            for(int i=0;i<size;i++){
+                auto p = q.front();
                 q.pop();
-                check(grid,u.first,u.second,n,m);
-            }
 
-            min++;
+                int x = p.first;
+                int y = p.second;
+
+                vector<pair<int,int>> dir ={{x+1,y},{x-1,y},{x,y+1},{x,y-1}};
+                for(auto d : dir){
+                    if(d.first>=0 && d.first<m && d.second<n && d.second>=0){
+                        if(grid[d.first][d.second]==1){
+                            grid[d.first][d.second]=2;
+                            q.push({d.first,d.second});
+                        }
+                    }
+                }
+
+            }
+            count++;
+
         }
+
         
-         for(int i=0 ;i<n ;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
                    return -1;
                 }
             }
         }
-        
-        return min;
+
+        return count;
     }
 };
