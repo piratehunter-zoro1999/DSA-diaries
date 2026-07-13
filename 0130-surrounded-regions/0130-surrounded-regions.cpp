5ef4ca8e0df3508623a7379bca5727ca038735c4
piratehunter-zoro1999 <kushpindersingh15@gmail.com>
2026-07-14 00:56:37 +0530

    Time: 0 ms (100%), Space: 14 MB (82.05%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,59 +1,37 @@
 class Solution {
 public:
-// aproach bfs ---> multi src
+void dfs(int i ,int j , vector<vector<char>> & board,int m,int n){
+
+    if(i<0 || j<0 || i>=m || j>=n || board[i][j]=='X'|| board[i][j]=='S') return;
+
+    board[i][j]='S';
+
+    dfs(i+1,j,board,m,n);
+    dfs(i-1,j,board,m,n);
+    dfs(i,j+1,board,m,n);
+    dfs(i,j-1,board,m,n);
+
+}
     void solve(vector<vector<char>>& board) {
-        int n=board.size();
-        int m=board[0].size();
-        queue<pair<int,int>> q;
-    //<--  --> left right
-        for(int i=0;i<n;i++){
+        int m= board.size();
+        int n=board[0].size();
 
-            if(board[i][0]=='O'){
-                board[i][0]='S';
-                q.push({i,0});
-            }
-
-            
-            if(board[i][m-1]=='O'){
-                board[i][m-1]='S';
-                q.push({i,m-1});
-            }
+        for(int i=0 ;i<m ;i++){
+            dfs(i,0,board,m,n);
+            dfs(i,n-1,board,m,n);
         }
-    // ^ v up down
-          for(int j=0;j<m;j++){
-
-            if(board[0][j]=='O'){
-                board[0][j]='S';
-                q.push({0,j});
-            }
-
-            
-            if(board[n-1][j]=='O'){
-                board[n-1][j]='S';
-                q.push({n-1,j});
-            }
-          
-          }
-        vector<pair<int,int>> dir={{0,1},{1,0},{-1,0},{0,-1}};
-        while(!q.empty()){
-            auto [i,j]=q.front();
-            q.pop();
-
-            for(auto[x,y] : dir){
-                int dx=i+x;
-                int dy=j+y;
-                
-                 if(dx>=0 && dy>=0 && dx<n &&dy<m && board[dx][dy]=='O'){
-                    board[dx][dy]='S';
-                    q.push({dx,dy});
-                 }
-            }
+        for(int j=0 ;j<n ;j++){
+            dfs(0,j,board,m,n);
+            dfs(m-1,j,board,m,n);
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(board[i][j]=='O') board[i][j]='X';
-                if(board[i][j]=='S') board[i][j]='O';
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='S'){
+                    board[i][j]='O';
+                }else{
+                    board[i][j]='X';
+                }
             }
         }
     }
