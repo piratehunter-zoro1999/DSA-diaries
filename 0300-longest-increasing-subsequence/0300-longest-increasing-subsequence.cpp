d29edd6767e66f881c54b44fd2af284738a2e2ad
piratehunter-zoro1999 <kushpindersingh15@gmail.com>
2026-09-07 13:04:45 +0530

    Time: 71 ms (53.71%), Space: 14.4 MB (48.33%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,18 +4,18 @@ public:
         
         int n = nums.size();
 
-        vector<int> dp(n);
-        dp[0]=1;
+        vector<int> dp(n,1);
+        
         
 
         for(int i =1;i<n;i++){
-            int candidate=1;
+            
            for(int j=0;j<i;j++){
               if(nums[j]<nums[i]){
-                 candidate=max(candidate,dp[j]+1);
+                 dp[i]=max(dp[i],dp[j]+1);
               }
            }
-           dp[i]=candidate;
+           
         }
 
         return *max_element(dp.begin(),dp.end());
