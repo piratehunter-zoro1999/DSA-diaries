fa487994bdcdce84100b3b545ad94ce2ca37b917
piratehunter-zoro1999 <kushpindersingh15@gmail.com>
2026-09-09 20:14:19 +0530

    Time: 114 ms (26.35%), Space: 13.3 MB (56.62%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int>dp(n+1,n+1);
+
+        dp[0]=0;
+        dp[1]=1;
+            for(int i=1;i<n+1;i++){
+                for(int j=1;j<=sqrt(n);j++){
+                    if(j*j<=i){
+                        dp[i]=min(dp[i],dp[i-j*j]+1);
+                    }
+                }
+            }
+
+        return dp[n];    
+        
+    }
+};
