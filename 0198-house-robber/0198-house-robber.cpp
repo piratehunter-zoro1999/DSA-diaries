de4ac8e67f5507ecc53028c2462a1054d444e7f6
piratehunter-zoro1999 <kushpindersingh15@gmail.com>
2026-09-14 22:28:19 +0530

    Time: 0 ms (100%), Space: 10.5 MB (64.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         
-        int n=nums.size();
-
-        if(n==1) return nums[0];
+        int n= nums.size();
 
         vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<n+1;i++){
+           dp[i]=max(dp[i-1],dp[i-2]+nums[i-1]);
+        }
 
-         dp[0]=0;
-         dp[1]=nums[0];
-         
-
-         for(int i=2;i<=n;i++){
-
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i-1]);
-         }
-
-         return dp[n];
+        return dp[n];
     }
 };
